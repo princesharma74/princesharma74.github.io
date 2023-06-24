db6ef165c5c1c5f41461bc2825dff5cb76f7fc77
Prince Sharma <princesharma2899@gmail.com>
2023-06-24 23:49:10 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,36 @@
 (function ($) {
+
   "use strict";
 
-  // Function to toggle dark mode
-  function toggleDarkMode() {
-    $('body').toggleClass('dark-mode');
-  }
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
 
-  // Add dark mode by default
-  $('body').addClass('dark-mode');
+    // HEADER
+    $(".navbar").headroom();
 
-  // COLOR MODE
-  $('.color-mode').click(function () {
-    $('.color-mode-icon').toggleClass('active');
-    toggleDarkMode();
-  });
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
 
-  // HEADER
-  $(".navbar").headroom();
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
 
-  // PROJECT CAROUSEL
-  $('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: true,
-    margin: 10,
-    nav: true
-  });
+    // TOOLTIP
+    $('.social-links a').tooltip();
 
-  // SMOOTHSCROLL
-  $(function () {
-    $('.nav-link, .custom-btn-link').on('click', function (event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - 49
-      }, 1000);
-      event.preventDefault();
-    });
-  });
-
-  // TOOLTIP
-  $('.social-links a').tooltip();
 })(jQuery);
